60b8d679629e24e9eed142041bcef2b4398dacff
Juan Antonio Lago <juan.antonio.lago.97@gmail.com>
2018-11-29 14:24:15 +0100

    Contador creado, Juan Antonio

---
+++ Entornos/src/contador/Principal.java
@@ -0,0 +1,20 @@
+package contador;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int cont = 1;
+		
+		do {
+			System.out.println(cont);
+			cont++;
+			
+		}while(cont != 101);
+		
+		
+		
+	}
+
+}
